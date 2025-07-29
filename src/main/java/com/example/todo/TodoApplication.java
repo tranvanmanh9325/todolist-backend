2bea708155e639b6e901745227c8a3e2fccbbe3a
tranvanmanh9325 <manhtrana1k45tl@gmail.com>
2025-07-29 17:30:08 +0700

    Add branch login

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(TodoApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
 }
