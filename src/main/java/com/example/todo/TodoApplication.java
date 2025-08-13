7ca47ba8d6266cc0c45b1697320be8905990cf53
tranvanmanh9325 <manhtrana1k45tl@gmail.com>
2025-08-13 08:35:06 +0700

    Thay thông tin bằng file .env

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -1,11 +1,21 @@
 package com.example.todo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoApplication {
     public static void main(String[] args) {
+        // Nạp file .env từ thư mục gốc
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing() // Không lỗi nếu file .env không tồn tại
+                .load();
+        // Đặt các biến môi trường từ file .env vào System properties
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        // Khởi động ứng dụng Spring Boot
         SpringApplication.run(TodoApplication.class, args);
     }
 }
